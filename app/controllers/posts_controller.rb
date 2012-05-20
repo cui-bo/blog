0b67809014dc76e3b1a607ba048437e9a12f4f5b
Bo CUI <bo.cui@supinfo.com>
2012-05-20 19:06:34 +0200

    A simple full version project built with rails 3

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  
+  http_basic_authenticate_with :name => 'bo', :password => 'bo',
+                              :except => [:index, :show]
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params[:post])
+    
+    puts @post
+    
+    if @post.save
+      redirect_to :action => :show, :id => @post.id
+      #render :text => params[:post].inspect
+    else
+      render 'new'
+    end
+    #render :text => params[:post].inspect
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update_attributes(params[:post])
+      redirect_to :action => :show, :id => @post.id
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to :action => :index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '57b518ed7e1db78fe8358828004e35447680a487265a560c42e8852a37e968da3a7f2d3929c6bb08f760d67f552efc5de80cc5b784a8dccb58f0b65c3d8d7508'
